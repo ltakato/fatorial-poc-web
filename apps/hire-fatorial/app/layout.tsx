ee8714de41d182a893eacbc16b6296bd637dd570
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-01-19 03:08:48 -0300

    feat: include metadata and public folder

--- apps/hire-fatorial/app/layout.tsx
+++ apps/hire-fatorial/app/layout.tsx
@@ -3,12 +3,20 @@ import { Inter } from "next/font/google";
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { Header } from "@/components/header";
+import { Metadata } from "next/types";
 
 const fontInter = Inter({
   subsets: ["latin"],
   display: "swap",
   weight: ["400", "500", "700"],
 });
+
+export const metadata: Metadata = {
+  robots: "all",
+  title: "HireAI",
+  icons: [{ rel: "icon", url: "favicon.ico" }],
+  description: "HireAI, a nova forma de contratar.",
+};
 
 export default function RootLayout({
   children,
